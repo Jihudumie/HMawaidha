a4ffa80aa85d985382030a3ffa1d935925a0555d
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-08-03 13:08:57 +0300

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -37,7 +37,7 @@ async def help_message_f(client, message):
     message_id = 99
     # display the /help message
     await message.reply_text(
-        f"Karibu: Mimi ni <a href='https://t.me/c/{channel_id}/{message_id}'>Uploader⬆</a> Nina Upload Audio/Video N.k\n\n1. /ytdl\n2. /leech",
+        f"   Karibu: \nMimi ni <a href='https://t.me/c/{channel_id}/{message_id}'>Uploader⬆</a> Nina Upload Audio/Video N.k\n\Reply Link kisha Tumia Command \n👇Hizin1. /ytdl\n2. /leech",
         quote=True
     )
 
